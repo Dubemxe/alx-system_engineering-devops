7ccd658bd0303fb986c23b7ae2d5a0e1580ac8b0
Chidubem Uchendu <dubemxe@gmail.com>
2024-01-29 10:58:11 -0800

    Done!

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This script retrieves employee data from API
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    tasks = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed_tasks = [t.get("title") for t in tasks if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(tasks)))
+    [print("\t {}".format(task)) for task in completed_tasks]
---
+++ 0x15-api/README.md
@@ -0,0 +1,5 @@
+REST_API
+Python over shell
+Export in different format
+Gather data from API
+Microservices
